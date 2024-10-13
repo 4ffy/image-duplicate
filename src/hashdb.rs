9eb3776eb32adc0a59534d361864570af03b442d
Cameron Norton <cameron.norton@gmail.com>
2024-10-13 14:40:40 -0700

    Remove HashEntry, make HashDB into a HashMap, add more args.

--- src/hashdb.rs
+++ src/hashdb.rs
@@ -1,6 +1,5 @@
-//! Structs and methods for dealing with a database of image hashes.
-//! [`HashEntry`] and [`HashDB`] form the main interface. `HashDB` provides
-//! methods to dump the database and read from a [MessagePack][rmp] file.
+//! Structs and methods for dealing with a database of image hashes. [`HashDB`]
+//! forms the main interface.
 
 use image_hasher::HasherConfig;
 use rmp_serde::{config::BytesMode, Serializer};
@@ -8,57 +7,19 @@ use serde::{
     de::{self, Visitor},
     Deserialize, Serialize,
 };
-use std::{collections::HashSet, fs, hash::Hash, path::Path};
+use std::{collections::HashMap, fs, hash::Hash, path::Path};
 use thiserror::Error;
 
-/// Wrapper around [`md5::Digest`] for serialization.
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Digest(md5::Digest);
+const SUFFIXES: [&str; 7] = ["bmp", "gif", "jpg", "jpeg", "jxl", "png", "webp"];
 
-impl From<md5::Digest> for Digest {
-    fn from(value: md5::Digest) -> Self {
-        Self(value)
-    }
-}
-
-impl Serialize for Digest {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_bytes(&self.0 .0)
-    }
-}
-
-impl<'de> Deserialize<'de> for Digest {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        deserializer.deserialize_bytes(DigestVisitor)
-    }
-}
-
-/// Helper for deserializing [`Digest`].
-struct DigestVisitor;
-
-impl<'de> Visitor<'de> for DigestVisitor {
-    type Value = Digest;
-
-    fn expecting(
-        &self, formatter: &mut std::fmt::Formatter,
-    ) -> std::fmt::Result {
-        formatter.write_str("eight bytes representing md5 digest")
-    }
-
-    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        match TryInto::<[u8; 16]>::try_into(v) {
-            Ok(v) => Ok(md5::Digest(v).into()),
-            Err(_) => Err(E::invalid_length(v.len(), &self)),
-        }
+fn has_image_suffix<P: AsRef<Path>>(file: P) -> bool {
+    // uhh...
+    match file.as_ref().extension() {
+        Some(x) => match x.to_str() {
+            Some(x) => SUFFIXES.contains(&x),
+            None => false,
+        },
+        None => false,
     }
 }
 
@@ -116,75 +77,31 @@ impl<'de> Visitor<'de> for ImageHashVisitor {
     }
 }
 
-/// Pair a filename along with its [md5][Digest] and [hash][ImageHash]. Entries
-/// are considered equivalent if their md5 hashes are equivalent.
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct HashEntry {
-    /// Canonicalized filename of the image that the entry is derived from.
-    pub filename: String,
+/// A database storing image hashes via an internal [`HashMap`].
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct HashDB(HashMap<String, ImageHash>);
 
-    /// md5 hash of the image.
-    pub md5: Digest,
+impl HashDB {
+    /// Create a new hash database.
+    pub fn new() -> Self {
+        HashDB(HashMap::new())
+    }
 
-    /// Perceptual hash of the image.
-    pub hash: ImageHash,
-}
-
-impl HashEntry {
-    /// Read an image file and form a `HashEntry`.
-    pub fn read_file<P: AsRef<Path>>(file: P) -> Result<Self, HashEntryError> {
+    /// Hash an image and insert it into the database.
+    pub fn insert_image<P: AsRef<Path>>(
+        &mut self, file: P,
+    ) -> Result<(), HashDBError> {
         // If I am to hash in parallel, each will need its own hasher, probably.
         let hasher = HasherConfig::new().to_hasher();
 
         let image = image::open(&file)?;
         let temp = image.resize(256, 256, image_hasher::FilterType::Nearest);
 
-        let filename = file.as_ref().canonicalize()?;
-        let md5 = md5::compute(&image.as_bytes());
+        let name = file.as_ref().canonicalize()?.to_string_lossy().into_owned();
         let hash = hasher.hash_image(&temp);
 
-        Ok(HashEntry {
-            filename: filename.to_string_lossy().into_owned(),
-            md5: md5.into(),
-            hash: hash.into(),
-        })
-    }
-}
-
-impl PartialEq for HashEntry {
-    fn eq(&self, other: &Self) -> bool {
-        self.md5 == other.md5
-    }
-}
-
-impl Eq for HashEntry {}
-
-impl Hash for HashEntry {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.md5.hash(state)
-    }
-}
-
-/// Errors that can happen when dealing with [`HashEntry`].
-#[derive(Debug, Error)]
-pub enum HashEntryError {
-    /// Wrapper around [`image::ImageError`].
-    #[error("could not read image: {0}")]
-    ImageError(#[from] image::ImageError),
-
-    /// Wrapper around [`std::io::Error`].
-    #[error("IO error: {0}")]
-    IOError(#[from] std::io::Error),
-}
-
-/// A database storing [`HashEntry`] items via an internal [`HashSet`].
-#[derive(Debug, Deserialize, PartialEq, Serialize)]
-pub struct HashDB(HashSet<HashEntry>);
-
-impl HashDB {
-    /// Create a new hash database.
-    pub fn new() -> Self {
-        HashDB(HashSet::new())
+        self.0.insert(name, hash.into());
+        Ok(())
     }
 
     /// Write the database to a [MessagePack][rmp] file.
@@ -204,29 +121,26 @@ impl HashDB {
         Ok(rmp_serde::from_read(fs::read(file)?.as_slice())?)
     }
 
-    /// Insert a [`HashEntry`] into the database. This defers to
-    /// [`HashSet::insert`] and thus has the same semantics.
-    pub fn insert(&mut self, entry: &HashEntry) -> bool {
-        self.0.insert(entry.clone())
-    }
-}
-
-impl FromIterator<HashEntry> for HashDB {
-    fn from_iter<T: IntoIterator<Item = HashEntry>>(iter: T) -> Self {
-        let mut db = HashDB::new();
-        for i in iter {
-            db.insert(&i);
+    /// Read image files from the given directory. Add entries for any images
+    /// that do not exist the database. Then, remove entries from the database
+    /// that no longer have any corresponding images on the filesystem.
+    pub fn read_dir<P: AsRef<Path>>(
+        &mut self, root: P,
+    ) -> Result<(), HashDBError> {
+        for entry in fs::read_dir(root)?
+            .filter_map(|x| x.ok())
+            .filter(|x| has_image_suffix(x.path()))
+        {
+            self.insert_image(entry.path())?;
         }
-        db
+        Ok(())
     }
-}
 
-impl IntoIterator for HashDB {
-    type Item = HashEntry;
-    type IntoIter = std::collections::hash_set::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
+    /// [`read_dir`][HashDB.read_dir] but scan the directory recursively.
+    pub fn read_dir_recursive<P: AsRef<Path>>(
+        &mut self, _root: P,
+    ) -> Result<(), HashDBError> {
+        unimplemented!()
     }
 }
 
@@ -240,6 +154,10 @@ pub enum HashDBError {
     /// Wrapper around [`rmp_serde::encode::Error`].
     #[error("could not encode database: {0}")]
     EncodeError(#[from] rmp_serde::encode::Error),
+
+    /// Wrapper around [`image::ImageError`]
+    #[error("could not read image: {0}")]
+    ImageError(#[from] image::ImageError),
 
     /// Wrapper around [`std::io::Error`].
     #[error("IO Error: {0}")]
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,10 @@
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use hashdb::{HashDB, HashEntry};
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
-use walkdir::WalkDir;
+use hashdb::HashDB;
+use std::path::PathBuf;
 
 pub mod hashdb;
-
-const SUFFIXES: [&str; 7] = ["bmp", "gif", "jpg", "jpeg", "jxl", "png", "webp"];
 
 /// Arguments to the image duplicate program.
 #[derive(Debug, Parser)]
@@ -21,53 +15,25 @@ pub struct Args {
     /// Directory to scan for images
     pub path: PathBuf,
 
-    /// Location of database file
-    #[arg(short, long)]
+    /// Location of database file (default: \<PATH\>/.image_hash.db)
+    #[arg(short = 'D', long)]
     pub db: Option<PathBuf>,
 
-    /// Scan directories for images recursively
-    #[arg(short, long)]
+    /// Scan directory for images recursively
+    #[arg(short = 'R', long)]
     pub recursive: bool,
-}
 
-fn has_image_suffix<P: AsRef<Path>>(file: P) -> bool {
-    // uhh...
-    match file.as_ref().extension() {
-        Some(x) => match x.to_str() {
-            Some(x) => SUFFIXES.contains(&x),
-            None => false,
-        },
-        None => false,
-    }
-}
+    /// Force rebuild hash database
+    #[arg(short = 'b', long)]
+    pub rebuild: bool,
 
-fn read_dir<P: AsRef<Path>>(root: P) -> Result<HashDB> {
-    Ok(fs::read_dir(root)?
-        .filter_map(|x| x.ok())
-        .filter_map(|x| {
-            let p = x.path();
-            match has_image_suffix(&p) && p.is_file() {
-                true => Some(p),
-                false => None,
-            }
-        })
-        .filter_map(|x| HashEntry::read_file(x).ok())
-        .collect())
-}
+    /// Do not dump hash database to file
+    #[arg(short = 'd', long)]
+    pub no_dump: bool,
 
-fn read_dir_recursive<P: AsRef<Path>>(root: P) -> Result<HashDB> {
-    Ok(WalkDir::new(root)
-        .into_iter()
-        .filter_map(|x| x.ok())
-        .filter_map(|x| {
-            let p = x.path();
-            match has_image_suffix(&p) && p.is_file() {
-                true => Some(p.to_owned()),
-                false => None,
-            }
-        })
-        .filter_map(|x| HashEntry::read_file(x).ok())
-        .collect())
+    /// Read database file only; do not update contents
+    #[arg(short = 'u', long, conflicts_with = "rebuild")]
+    pub no_update: bool,
 }
 
 /// Run the image duplicate program.
@@ -77,22 +43,30 @@ pub fn run(args: &Args) -> Result<()> {
         return Err(anyhow!("Directory not found: {:?}", root));
     }
 
-    let db = match &args.db {
+    let db_file = match &args.db {
         Some(path) => path.clone(),
         None => root.join(".image_hash.db"),
     };
 
-    let hashdb = match args.recursive {
-        true => read_dir_recursive(&root)?,
-        false => read_dir(&root)?,
+    let mut hashdb = match db_file.is_file() && !args.rebuild {
+        true => HashDB::from_file(&db_file)?,
+        false => HashDB::new(),
     };
 
-    hashdb.to_file(&db)?;
-    let hashdb2 = HashDB::from_file(&db)?;
+    if !args.no_update {
+        match args.recursive {
+            true => hashdb.read_dir_recursive(root)?,
+            false => hashdb.read_dir(root)?,
+        }
+    }
 
     dbg!(&hashdb);
-    dbg!(&hashdb2);
-    assert_eq!(&hashdb, &hashdb2);
+
+    if !args.no_dump {
+        hashdb.to_file(&db_file)?;
+        let hashdb2 = HashDB::from_file(&db_file)?;
+        assert_eq!(&hashdb, &hashdb2);
+    }
 
     Ok(())
 }
