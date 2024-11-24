a3a4648e1e475d5a877c018ca991c3f6c75ed38c
Cameron Norton <cameron.norton@gmail.com>
2024-11-24 14:48:17 -0800

    Add LICENSE and blurbs.

--- src/hashdb.rs
+++ src/hashdb.rs
@@ -1,3 +1,19 @@
+// image-dupicate - GUI for handling visually similar images in a directory
+// Copyright (C) 2024 Cameron Norton
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //! Structs and methods for dealing with a database of image hashes. [`HashDB`]
 //! forms the main interface. `HashDB` is backed by a [`HashMap`] and supports
 //! hashing image files as well as reading and writing to Zlib'd
